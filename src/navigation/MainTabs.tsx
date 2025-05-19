58bcf0989165d01ab70f9cfab29d513849e3a8d8
Juani <juan.ferrer@overproofdev.com>
2025-05-19 19:33:29 -0300

    Added tab bar styling.

--- src/navigation/MainTabs.tsx
+++ src/navigation/MainTabs.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Cart from '../screens/Cart';
+import FIcon from 'react-native-vector-icons/Feather';
+import { colors } from '../theme';
+import { TouchableOpacity } from 'react-native';
 
 export type TabParamList = {
     Home: undefined;
@@ -11,9 +14,28 @@ export type TabParamList = {
 const Tab = createBottomTabNavigator<TabParamList>();
 
 const MainTabs = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
+  <Tab.Navigator screenOptions={({ route }) => ({
+    tabBarStyle: {
+      backgroundColor: '#fff',
+      position: 'absolute',
+      height: 65,
+      paddingTop: 5,
+      borderTopWidth: 1.5,
+      borderTopColor: colors.secondary,
+    },
+    tabBarButton: (props) => <TouchableOpacity {...props} />,
+    tabBarActiveTintColor: colors.primary,
+    tabBarInactiveTintColor: colors.secondary,
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+      if (route.name === 'Home') {iconName = 'home';}
+      else if (route.name === 'Cart') {iconName = 'shopping-cart';}
+      return <FIcon name={iconName} size={size} color={focused ? colors.primary : colors.secondary} />;
+    },
+    headerShown: false,
+  })}>
     <Tab.Screen name="Home" component={Home} options={{ title: 'Home' }} />
-    <Tab.Screen name="Cart" component={Cart} options={{ title: 'Profile' }} />
+    <Tab.Screen name="Cart" component={Cart} options={{ title: 'Cart' }} />
   </Tab.Navigator>
 );
 
---
+++ src/theme/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: '#1A1A1A',
+    secondary: '#999999',
+    lightGrey: '#E6E6E6',
+};
